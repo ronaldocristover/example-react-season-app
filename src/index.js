bece03790558404c50f370c0bc055b6867b41f01
ronaldocristover <ronaldochristover@gmail.com>
2020-03-31 09:42:31 +0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
+class App extends React.Component {
+
+    state = {lat : null, errorMessage : ''}
+
+    componentDidMount(){
+        console.log('Component rendered to screed');
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat : position.coords.latitude}),
+            err => this.setState({errorMessage : err.message})
+        );
+    }
+
+    componentDidUpdate(){
+        console.log('Component was just updated');
+    }
+
+    renderContent(){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error : {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat){ 
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <Spinner message="Please Accept location Request!"/>
+    }
+
+    render() {
+        
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
+    }
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <App />, 
+    document.querySelector('#root')
+); 
